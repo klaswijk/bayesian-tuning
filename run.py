73944207c18c8936648449f38b7f55ef0986a8d7
Klas Wijk <kwijk@kth.se>
2021-03-26 17:21:48 +0100

    update -h text

--- run.py
+++ run.py
@@ -6,29 +6,29 @@ from tuning import run_tuning
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-r', '--random', action='store_true')
-    group.add_argument('-b', '--bayesian', action='store_true')
+    group.add_argument('-r', '--random', action='store_true', help='use Random Search')
+    group.add_argument('-b', '--bayesian', action='store_true', help='use Bayesian Search')
     parser.add_argument('-t', '--tsp',
-        help='The path of the TSPLIB instance to run',
+        help='the path of the TSPLIB instance to run',
         required=True
     )
     parser.add_argument('-i', '--n_iterations',
-        help='The number of acotsp iterations to run',
+        help='the number of acotsp iterations to run',
         type=int,
         required=True
     )
     parser.add_argument('-c', '--n_calls',
-        help='The number of acotsp calls that the optimization function makes',
+        help='the number of acotsp calls that the optimization function makes',
         type=int,
         required=True
     )
     parser.add_argument('-n', '--n_runs',
-        help='The number of repeated runs',
+        help='the number of repeated runs',
         type=int,
         required=True
     )
     parser.add_argument('-p', '--n_procs',
-        help='The number of processes to run in parallell (default: #cores)',
+        help='the number of processes to run (default: #cores)',
         type=int,
         default=cpu_count()
     )
